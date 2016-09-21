78703900cc0dcaa33fd8d869c0c793c895982d4b
baiwei <baiwei@baiweideMac.local>
2016-09-21 08:12:02 +0800

    company

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -1,10 +1,13 @@
 <?php
 namespace app\index\controller;
+use think\View;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
     public function index()
     {
+        $view = new view();
         return $view->fetch('Index/index');
     }
 }
