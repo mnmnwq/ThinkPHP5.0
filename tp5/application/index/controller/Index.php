8c6abaeaa2fa1ecaf0077440891c7f11a5c2e0b8
baiwei <baiwei@baiweideMac.local>
2016-09-22 16:54:07 +0800

    company

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -2,7 +2,6 @@
 namespace app\index\controller;
 use think\Request;
 use think\View;
-use think\Controller;
 
 class Index extends Common
 {
@@ -14,23 +13,6 @@ class Index extends Common
     public function index()
     {
         $request = Request::instance();
-        /*echo '<br/>';
-        echo $request->domain().'<br/>';
-        echo $request->baseFile().'<br/>';
-        echo $request->url().'<br/>';
-        echo $request->url(true).'<br/>';
-        echo $request->baseUrl().'<br/>';
-        echo $request->root().'<br/>';
-        echo $request->root(true).'<br/>';
-        echo $request->pathinfo().'<br/>';
-        echo $request->path().'<br/>';
-        echo $request->ext().'<br/>';
-        echo $request->ip().'<br/>';
-        $head_info = $request->header();
-        var_dump($head_info);
-        //$request->param();
-        $post = $request->except(['user_name']);
-        var_dump($post);*/
         $view = new view();
         return $view->fetch('index');
     }
